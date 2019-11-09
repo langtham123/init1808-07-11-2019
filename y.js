ba3158b891c08485134eb602528dacdeac27b8b8
langtham123 <wanbi2893tuan@gmail.com>
2019-11-09 10:49:34 +0700

    swtch case

---
+++ y.js
@@ -0,0 +1,16 @@
+let a = 3;
+switch (a) {
+    case 1:
+        console.log('Mot');
+        break;
+    case 2:
+        console.log('Hai');
+        break;
+    case 3:
+        console.log('Ba');
+        break;
+
+    default:
+        console.log('KHONG TIM THAY');
+        break;
+}
